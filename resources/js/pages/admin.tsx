a54064b12264434feea0a3fe5cdd4785c95bb7ce
kimpoy31 <pinanonangkim000@gmail.com>
2025-08-04 08:43:53 +0800

    add a go back button

--- resources/js/pages/admin.tsx
+++ resources/js/pages/admin.tsx
@@ -1,5 +1,6 @@
 import { User } from '@/types';
 import { Event } from '@/types/types';
+import { MoveLeft } from 'lucide-react';
 import JudgesComponent from './_components/JudgesComponent';
 import ParticipantsComponent from './_components/ParticipantsComponent';
 
@@ -12,8 +13,17 @@ const admin = ({ event, judges_to_choose_from }: PageProps) => {
     return (
         <div className="flex h-screen">
             <div className="w-full max-w-md overflow-auto bg-base-200 p-4 shadow-lg">
+                <button
+                    className="btn btn-sm"
+                    onClick={() => {
+                        window.history.back();
+                    }}
+                >
+                    <MoveLeft />
+                </button>
+                <div className="divider" />
                 <JudgesComponent event={event} judges={judges_to_choose_from} />
-                <div className="divider"></div>
+                <div className="divider" />
                 <ParticipantsComponent event={event} />
             </div>
             <div className="w-full overflow-auto"></div>
